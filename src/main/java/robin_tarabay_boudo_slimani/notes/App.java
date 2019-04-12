9c6cda3bcf05d8273ea490abe50c81c828c07d64
mortexsa <slimani-arezki@hotmail.com>
2019-04-12 13:06:54 +0200

    premier commit: initialisation du projet maven

---
+++ src/main/java/robin_tarabay_boudo_slimani/notes/App.java
@@ -0,0 +1,13 @@
+package robin_tarabay_boudo_slimani.notes;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
