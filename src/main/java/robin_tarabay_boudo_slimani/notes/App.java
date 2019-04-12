3f54cfd3a58309dd71525c6a7b972c4d7f59bfea
mortexsa <slimani-arezki@hotmail.com>
2019-04-12 16:15:28 +0200

    le pom.xml est fonctionnel, nous pouvons commencer a programmer le projet

--- src/main/java/robin_tarabay_boudo_slimani/notes/App.java
+++ src/main/java/robin_tarabay_boudo_slimani/notes/App.java
@@ -1,13 +1,40 @@
 package robin_tarabay_boudo_slimani.notes;
 
-/**
+import java.lang.reflect.Field;
+import java.lang.reflect.Method;
+import static org.asciidoctor.Asciidoctor.Factory.create;
+import org.asciidoctor.Asciidoctor;
+import java.util.Collections;
+/*
  * Hello world!
  *
  */
+
+
 public class App 
 {
+	@SuppressWarnings("unchecked")
+    public static void disableAccessWarnings() {
+        try {
+            Class unsafeClass = Class.forName("sun.misc.Unsafe");
+            Field field = unsafeClass.getDeclaredField("theUnsafe");
+            field.setAccessible(true);
+            Object unsafe = field.get(null);
+
+            Method putObjectVolatile = unsafeClass.getDeclaredMethod("putObjectVolatile", Object.class, long.class, Object.class);
+            Method staticFieldOffset = unsafeClass.getDeclaredMethod("staticFieldOffset", Field.class);
+
+            Class loggerClass = Class.forName("jdk.internal.module.IllegalAccessLogger");
+            Field loggerField = loggerClass.getDeclaredField("logger");
+            Long offset = (Long) staticFieldOffset.invoke(unsafe, loggerField);
+            putObjectVolatile.invoke(unsafe, loggerClass, offset, null);
+        } catch (Exception ignored) {
+        }
+    }
     public static void main( String[] args )
     {
-        System.out.println( "Hello World!" );
+    	disableAccessWarnings();
+    	Asciidoctor asciidoctor = create();
+
     }
 }
