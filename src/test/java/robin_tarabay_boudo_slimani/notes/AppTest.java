5a3fe72ee116331bf904f9b8d4dbdf2aacd34885
BOUDO <boudoibrahim@gmail.com>
2019-04-13 23:29:43 +0200

    structure Junit 4

--- src/test/java/robin_tarabay_boudo_slimani/notes/AppTest.java
+++ src/test/java/robin_tarabay_boudo_slimani/notes/AppTest.java
@@ -1,38 +1,18 @@
 package robin_tarabay_boudo_slimani.notes;
 
-import junit.framework.Test;
-import junit.framework.TestCase;
-import junit.framework.TestSuite;
+import static org.junit.Assert.*;
+import org.junit.*;
+
 
 /**
  * Unit test for simple App.
  */
 public class AppTest 
-    extends TestCase
 {
-    /**
-     * Create the test case
-     *
-     * @param testName name of the test case
-     */
-    public AppTest( String testName )
+	@Test
+    public void Test()
     {
-        super( testName );
+        assertTrue(true);
     }
 
-    /**
-     * @return the suite of tests being tested
-     */
-    public static Test suite()
-    {
-        return new TestSuite( AppTest.class );
-    }
-
-    /**
-     * Rigourous Test :-)
-     */
-    public void testApp()
-    {
-        assertTrue( true );
-    }
 }
