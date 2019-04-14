7126c19672133609c722697855dc80dbef3113d8
BOUDO <boudoibrahim@gmail.com>
2019-04-14 19:53:16 +0200

    modification pour tester la commande view

--- src/main/java/robin_tarabay_boudo_slimani/notes/App.java
+++ src/main/java/robin_tarabay_boudo_slimani/notes/App.java
@@ -18,24 +18,24 @@ public class App
 	/**
 	 * Fonction permettant de supprimer les warnings
 	 */
-	@SuppressWarnings("unchecked")
-    public static void disableAccessWarnings() {
-        try {
-            Class unsafeClass = Class.forName("sun.misc.Unsafe");
-            Field field = unsafeClass.getDeclaredField("theUnsafe");
-            field.setAccessible(true);
-            Object unsafe = field.get(null);
-
-            Method putObjectVolatile = unsafeClass.getDeclaredMethod("putObjectVolatile", Object.class, long.class, Object.class);
-            Method staticFieldOffset = unsafeClass.getDeclaredMethod("staticFieldOffset", Field.class);
-
-            Class loggerClass = Class.forName("jdk.internal.module.IllegalAccessLogger");
-            Field loggerField = loggerClass.getDeclaredField("logger");
-            Long offset = (Long) staticFieldOffset.invoke(unsafe, loggerField);
-            putObjectVolatile.invoke(unsafe, loggerClass, offset, null);
-        } catch (Exception ignored) {
-        }
-    }
+//	@SuppressWarnings("unchecked")
+//    public static void disableAccessWarnings() {
+//        try {
+//            Class unsafeClass = Class.forName("sun.misc.Unsafe");
+//            Field field = unsafeClass.getDeclaredField("theUnsafe");
+//            field.setAccessible(true);
+//            Object unsafe = field.get(null);
+//
+//            Method putObjectVolatile = unsafeClass.getDeclaredMethod("putObjectVolatile", Object.class, long.class, Object.class);
+//            Method staticFieldOffset = unsafeClass.getDeclaredMethod("staticFieldOffset", Field.class);
+//
+//            Class loggerClass = Class.forName("jdk.internal.module.IllegalAccessLogger");
+//            Field loggerField = loggerClass.getDeclaredField("logger");
+//            Long offset = (Long) staticFieldOffset.invoke(unsafe, loggerField);
+//            putObjectVolatile.invoke(unsafe, loggerClass, offset, null);
+//        } catch (Exception ignored) {
+//        }
+//    }
 	
 	/**
 	 * Fonction principale du programme 
@@ -43,10 +43,12 @@ public class App
 	 */
     public static void main( String[] args )
     {
-    	disableAccessWarnings();
-    	Asciidoctor asciidoctor = create();
+//    	disableAccessWarnings();
+//    	Asciidoctor asciidoctor = create();
     	
     	Notes test = new Notes.NoteBuilder("Ma première Note").build();
     	System.out.println(test.toString());
+    	ViewNotesCommand cmd = new ViewNotesCommand("Test");
+    	cmd.execute();
     }
 }
